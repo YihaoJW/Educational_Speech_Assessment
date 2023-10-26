6c4e1ac8bfb8472be58ba5ebec2b2faaba8e0363
Wang,Yihao <13759885+YihaoJW@users.noreply.github.com>
2023-10-26 02:22:28 -0500

    Correct Tensorboard Path

--- DataPreperation/training.py
+++ DataPreperation/training.py
@@ -77,12 +77,11 @@ if __name__ == '__main__':
         with open(config['model_storage']['model_restore'].parent / 'wandb_id.txt', 'w') as f:
             f.write(run_id)
 
+    wandb.tensorboard.patch(root_logdir=config['model_storage']['tensorboard_path'])
     wandb.init(project="ASR_Model_AttentionBased",
                config=config['model_setting'],
                resume="allow", id=run_id,
-               dir=config['model_storage']['model_restore'].parent,
-               sync_tensorboard=True,
-               )
+               dir=config['model_storage']['model_restore'].parent)
     print("manual debug: Wandb created")
     # create learning rate scheduler
     lr_config = config['training_setting']['learning_rate']
